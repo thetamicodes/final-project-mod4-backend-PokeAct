9de76b7808d935ef8fbcdb4b89a30c6bf362f1a0
github username <github email address>
2019-11-28 19:15:33 +0000

    Players controller created and cors configuration added

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,23 @@
 class PlayersController < ApplicationController
+   
+    def index
+        players = Player.All
+        render json: palyers.to_json
+    end
+
+    def show
+        player = Player.find(params[:id])
+        render json: player.to_json
+    end
+
+    def create
+        player = Player.create(player_params)
+        render json: player, status: :created
+    end
+
+    private 
+    def player_params
+        params.require(:player).permit(:username)
+    end
+
 end
