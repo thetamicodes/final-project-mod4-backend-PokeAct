b874e75695deb79adbfa62e98758a95928e8b0f3
github username <github email address>
2019-11-29 09:31:05 +0000

    typos fixed

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,18 +1,19 @@
 class PlayersController < ApplicationController
    
     def index
-        players = Player.All
-        render json: palyers.to_json
+        players = Player.all
+        render json: players.to_json
     end
 
     def show
         player = Player.find(params[:id])
-        render json: player.to_json
+        render json: player
     end
 
     def create
         player = Player.create(player_params)
         render json: player, status: :created
+        debugger
     end
 
     private 
