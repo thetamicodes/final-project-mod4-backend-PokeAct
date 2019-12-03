7a23aa79adf3e2fc3d359e23f142d5d6df0a3d3f
thetamicodes <reichold.tamara@gmail.com>
2019-12-03 19:12:17 +0000

    highscore route

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,27 @@
 class GamesController < ApplicationController
+
+    def index
+        games = Game.all
+        render json: games.to_json
+    end
+
+    def show
+        game = Game.find(params[:id])
+        render json: game
+    end
+
+    def create
+        game = Game.create(game_params)
+        render json: game, status: :created
+    end
+
+    def highscore
+        games = Game.highscores_with_names
+        render json: games
+    end 
+
+    private 
+    def game_params
+        params.require(:game).permit(:score, :player_id)
+    end
 end
