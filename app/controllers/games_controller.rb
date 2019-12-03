1deae19f51ec4ebc6ad009332dae7e996d0ef1e0
github username <github email address>
2019-12-03 19:14:07 +0000

    merge

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,22 @@
 class GamesController < ApplicationController
+
+    def index
+        games = game.all
+        render json: games.to_json
+    end
+
+    def show
+        game = games.find(params[:id])
+        render json: games
+    end
+
+    def create
+        game = games.create(games_params)
+        render json: games, status: :created
+    end
+
+    private 
+    def games_params
+        params.require(:game).permit(:score, :player_id)
+    end
 end
