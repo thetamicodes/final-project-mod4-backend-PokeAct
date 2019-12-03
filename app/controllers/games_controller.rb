7d10ae8ada223aa0479e31bb42efb6be45d4d16e
thetamicodes <reichold.tamara@gmail.com>
2019-12-03 19:20:27 +0000

    working backend

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,28 +1,11 @@
 class GamesController < ApplicationController
 
     def index
-<<<<<<< HEAD
-        games = game.all
-=======
         games = Game.all
->>>>>>> 7a23aa79adf3e2fc3d359e23f142d5d6df0a3d3f
         render json: games.to_json
     end
 
     def show
-<<<<<<< HEAD
-        game = games.find(params[:id])
-        render json: games
-    end
-
-    def create
-        game = games.create(games_params)
-        render json: games, status: :created
-    end
-
-    private 
-    def games_params
-=======
         game = Game.find(params[:id])
         render json: game
     end
@@ -39,7 +22,6 @@ class GamesController < ApplicationController
 
     private 
     def game_params
->>>>>>> 7a23aa79adf3e2fc3d359e23f142d5d6df0a3d3f
         params.require(:game).permit(:score, :player_id)
     end
 end
