f7dd79b8a59c77a530a76ea9b7ff00354fd401a0
thetamicodes <reichold.tamara@gmail.com>
2019-12-05 10:57:38 +0000

    reset data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-p1 = Player.create(username: "Sarah")
+# p1 = Player.create(username: "Sarah")
 
-g1 = Game.create(score: 100, player_id: p1.id)
+# g1 = Game.create(score: 100, player_id: p1.id)
 
-c1 = Card.create(img_url: "Example")
+# c1 = Card.create(img_url: "Example")
 
-j1 = GameCard.create(game_id: g1.id, card_id: c1.id)
+# j1 = GameCard.create(game_id: g1.id, card_id: c1.id)
 
 
 # p1.games
